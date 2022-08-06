f9ef74821bad22a31236f7fe2bb6108ab7286631
bcshelto565 <104048251+bcshelto565@users.noreply.github.com>
2022-08-05 22:35:45 -0500

    Update WA.cpp
    Add in new functions which are commented out as their current versions prevent compiling due to call issues. Need to write simpler versions of the functions such as the task 3 function, route 3 finder, and adjChck functions which are used for the task 3 prompt, route finding and building and the adjacency check for two given int values.

--- WA.cpp
+++ WA.cpp
@@ -5,8 +5,36 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include <stack>
+#include <set>
 
 using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node *next;
+        Node(Node* head, int data);
+        Node newNode(Node* previous, int data);
+        void printList(Node *node);
+};
+
+Node::Node(Node* head, int data){
+    new Node* n5;
+    n5->val = data;
+    head->next = n5;
+};
+
+Node Node::newNode(Node* previous, int data){
+    if(previous == NULL){
+        cout << "Previous node cannot be NULL \n";
+        return *previous;
+    }
+    Node* newnode = new Node();
+    newnode->val = data;
+    newnode->next = previous->next;
+    previous->next = newnode;
+}
 
 class Graph{
      public:
@@ -16,6 +44,8 @@ class Graph{
           void print();
           void addEdge(int node1, int node2){ adj[node1][node2] = 1; };
           bool adjChck(string n1, string n2);
+          int routeFinder(Node* head, int in, int out, stack<int> stack, set<int> set);
+          void task3(map<const string, int> map1, Graph g);
      private: 
           int n;
           int adj[140][140]; // You may want to use adjacent list implementation for sparse graph instead
@@ -72,8 +102,57 @@ bool Graph::adjChck(string n1, string n2){
             return false;
         }
     }
-    
 }
+
+int Graph::routeFinder(Node* head, int in, int out, stack<int> stack, set<int> set){
+    if(adj[in][out]){
+        if(set.find(out) != set.end()){
+            cout << stack.top() << " is being popped as the top element for being in the stack\n";
+            stack.pop();
+        }
+        return 0;
+    }
+    else{
+        for(int e=0; e<140; e++){
+            if(adj[in][e] && adj[e][out]){
+                head->next = Node(head, e);
+                cout << e << " is a connection between " << in << " and " << out << endl;
+                return e;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
+
+// map<const string, int> city;
+
+/* void Graph::task3(map<const string, int> map1, Graph g){
+    string origin;
+    int originI;
+    stack<int> destinations;
+    set<int> priorities;
+    cout << "What is the city that we are using as the origin point? (case sensitive): ";
+    getline(cin, origin);
+    originI = city[origin];
+    for(int d=0; d<n; n++){
+        if(adj[originI][d]){
+            destinations.push(d);
+            priorities.insert(d);
+        }
+    }
+    int desSize = destinations.size();
+    // routefinder(originI, destinations.top(), destinations);
+    // Node* head = NULL;
+    Node* head = NULL;
+    head->val = originI;
+    graph.routeFinder(head, originI, destinations.top(), destinations, priorities);
+} */
+
+/* int routefinder(int in, int out, stack<int> stack){
+    
+}*/
 
 
 struct strCmp {
@@ -84,21 +163,24 @@ struct strCmp {
 
 
 /*to map a city name with an unique integer */
-map<const string, int> city;
+
 
 int main(int argc, char *argv[]){
-   int i, j, node1, node2, n;
-   string line, n3;
+   int i, j, node1, node2, n, ptask;
+   string line, n3, task;
 
 
    cout << "Please enter the number of cities in your graph: " << endl;
    cout << "---------------------------------------------------" << endl;
    getline(cin, n3);
    n = stoi(n3);
+   cout << "What task are we currently testing for? (1-4): ";
+   getline(cin, task);
+   ptask = stoi(task);
    //cin >> n;
    //n = 140;
     Graph graph(n);
-    string cityg1, cityg2, test;
+    /*string cityg1, cityg2, test;
     cout << "test test\n";
     getline(cin, test);
     //cin >> test;
@@ -107,7 +189,7 @@ int main(int argc, char *argv[]){
     //cin >> cityg1;
     cout << "What are your two cities? city 2: ";
     getline(cin, cityg2);
-    //cin >> cityg2; 
+    //cin >> cityg2; */
    char lineChar[256];
 
    ifstream cityFile;
@@ -139,10 +221,10 @@ int main(int argc, char *argv[]){
           line.erase(0,7);
 	  printf("%s\n", line.c_str());
           if(city.find(line) != city.end()){
-	  	cout << "locate!" << line <<  line.length() << endl;
+	  	//cout << "locate!" << line <<  line.length() << endl;
 	  }
 	  else{
-	  	cout << "NO!" << line <<  line.length() << endl;
+	  	//cout << "NO!" << line <<  line.length() << endl;
 	  }
 	  //cout << line << " " << city[line] << endl;
           node1 = city[line];
@@ -153,14 +235,22 @@ int main(int argc, char *argv[]){
       }
    }
    flightFile.close();
-   if(graph.adjChck(cityg1, cityg2)){
+   if(ptask = 3){
+        // graph.task3(city, graph); this line won't work due to an issue with the "graph." part
+   }
+   /*if(graph.adjChck(cityg1, cityg2)){
         cout << cityg1 << " and " << cityg2 << " are adjacent!! True!!\n";
    }
    else{
         cout << cityg1 << " and " << cityg2 << " are not adjacent!! False!!\n";
    }
    /* print the graph */
+
+    // --------------------------------------------------------------------------------
+   // graph printout is below, this should be uncommented out for the final submission
+   // ----------------------------------------------------------------------------------
+
    cout << endl << "The graph generated can be represented by the following adjacent matrix : " << endl;
    cout << "-----------------------------------------------------------------------------------" << endl;
-   graph.print();
+   // graph.print();
 }
