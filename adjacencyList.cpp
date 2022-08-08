989e6ef7bfc7467d843fd7cde9626997197300f6
FireAndIrons <67036557+FireAndIrons@users.noreply.github.com>
2022-08-07 23:37:34 -0500

    Update adjacencyList.cpp
    Redid the main data structure to become more of an abstract class - this was helpful to discover bugs and explicitly define the structure better

--- adjacencyList.cpp
+++ adjacencyList.cpp
@@ -8,83 +8,172 @@
 #include <fstream>
 #include <unordered_map>
 #include <list>
+#include <queue>
 using namespace std;
 
-class Graph {
+/*
+ * City class: defines a city's: name, edges, parent, and holds a bool for discovery
+ */
+class City {
+public:
+    City(string newName) {              //constructor
+        name = newName;
+    }
+    unordered_map<City*, int> edgeList; //edge list <City type pointer, int for weight>
+    string name;                        //name of city
+    bool discovered;                    //discovery bool for marking a node as discovered or not
+    City* parentCity;                   //city type pointer to point at city's parent node
+};
 
-    unordered_map<string, list<pair<string, int>>> cityAdjList; //adjacency list using stl unordered_map
-                                                                //it is of key <<string> -> list<string, int>>
+
+class Graph {
+    unordered_map<string, City*> graph; //graph list <string, City type pointer>
+
+private:
+    int countConnectionsDepthFirst(City* cityA, City* cityB, int numConnections, int recursionDepth) {
+        auto search = cityA->edgeList.find(cityB);
+
+        //we have found our destination, so return the number of connections
+        if(search != cityA->edgeList.end()) {
+            return recursionDepth + 1;
+        }
+        else if(recursionDepth < numConnections - 1){
+            for(auto i: cityA->edgeList) {
+               int temp = countConnectionsDepthFirst(i.first, cityB, numConnections, recursionDepth + 1);
+
+               if(temp != -1) {
+                   return temp;
+               }
+            }
+        }
+        //no route found
+        return -1;
+    }
+
+    int countConnectionsBreadthFirst(City* cityA, City* cityB, int numConnections) {
+        queue<City*> q;
+
+        //make all cities undiscovered
+        for(auto city: graph) {
+            city.second->discovered = false;
+        }
+
+        //make first city discovered
+        cityA->discovered = true;
+
+        //push onto stack
+        q.push(cityA);
+
+        while(!q.empty()) {
+            City* temp = q.front();
+            if(temp == cityB) {
+                int countPath = 0;
+
+                //start at our leaf, stop when we've found the root, crawl back up until we reach root
+                for(City* childNode = temp; childNode != cityA; childNode = childNode->parentCity) {
+                    countPath++;
+                }
+
+                if(countPath < numConnections) {
+                    return countPath;
+                }
+                else {
+                    return -1;
+                }
+            }
+
+            q.pop();
+
+            for(auto nbr: temp->edgeList) {
+                if(!nbr.first->discovered) {
+                    nbr.first->discovered = true;
+                    nbr.first->parentCity = temp;
+                    q.push(nbr.first);
+                }
+            }
+        }
+
+        return -1;
+    }
 
 public:
     void addEdge(string sourceCity, string destinationCity, int weight) {
 
-        //sourceCity -> destinationCity, weight; pushback the pair of destin and weight at the source
-        cityAdjList[sourceCity].push_back(make_pair(destinationCity, weight));
+        auto search = graph.find(sourceCity);
+
+        //failed to find source city
+        if(search == graph.end()) {
+            City* newCity = new City(sourceCity);
+            graph.insert(make_pair(sourceCity, newCity));
+        }
+
+        search = graph.find(destinationCity);
+
+        //failed to find destination city
+        if(search == graph.end()) {
+            City* newCity = new City(destinationCity);
+            graph.insert(make_pair(destinationCity, newCity));
+        }
+
+        graph[sourceCity]->edgeList.insert(make_pair(graph[destinationCity], weight));
 
     }
 
     //print list function
     void printAdjList() {
-        for(auto p: cityAdjList) {
-            string city = p.first;
-            list<pair<string, int>> neighbors = p.second;
+        for(auto p: graph) {
+            string cityName = p.first;
+            City* city = p.second;
 
-            cout << "Source: " << city << " --> " << endl;
-            for(auto nbr: neighbors) {
-                string destination = nbr.first;
+            cout << "Source: " << city->name << " --> " << endl;
+            for(auto nbr: city->edgeList) {
+                City* destinationCity = nbr.first;
                 int weight = nbr.second;
 
-                cout << destination << " " << weight << endl;
+                cout << destinationCity->name << " " << weight << endl;
             }
             cout << endl;
         }
     }
 
-    void testOne(string cityA, string cityB, int numConnections) {
-        int countWeight = 1; //countWeight starts at 1 because all edges have weight = 1
+    void testOne(string cityAName, string cityBName, int numConnections) {
+        auto search = graph.find(cityAName);
 
-        //checking if city was found or not
-        if(cityAdjList.find(cityA) == cityAdjList.end()) {
-            cout << cityA << " was not found" << endl;
+        //failed to find source city
+        if(search == graph.end()) {
+            cout << cityAName << "not found." << endl;
+            return;
         }
-        //walking through graph to find cityA and cityB
-        else {
-            for (auto key: cityAdjList) {
-                string city = key.first;
-                list<pair<string, int>> neighbors = key.second;
 
-                if (city == cityA) {
-                    for (auto nbr: neighbors) {
-                        string destination = nbr.first;
-                        int weight = nbr.second;
+        search = graph.find(cityBName);
 
-                        //count weight until destination is reached
-                        if(destination == cityB) {
-                            break;
-                        }
-                        else {
-                            countWeight = weight + countWeight;
-                        }
-                    }
-                }
-            }
+        //failed to find source city
+        if(search == graph.end()) {
+            cout << cityBName << "not found." << endl;
+            return;
         }
+
+        City* cityA = graph[cityAName];
+        City* cityB = graph[cityBName];
+
+        //call the BFS function to count connections between cityA and cityB
+        int jumps = countConnectionsBreadthFirst(cityA, cityB, numConnections);
 
         //reporting to user if connection is in x or less connections
-        if(numConnections >= countWeight) {
-            cout << cityA << " to " << cityB << endl;
-            cout << "Total connections: " << countWeight << endl;
-        }
-        else {
+        if(jumps == -1) {
             cout << "No route exists." << endl;
         }
-
+        else {
+            cout << cityA->name << " to " << cityB->name << endl;
+            cout << "Total connections: " << jumps << endl;
+        }
     }
 
     void testTwo() {
-        //check to see if key even has those cities?
-        //count edges
+
     }
+
+
 };
 
 
@@ -114,8 +203,15 @@ int main() {
 
     inputFile.close();
 
-    worldAirline.testOne("Seoul, South Korea", "Seattle, United States", 7);
-    worldAirline.testOne("Osaka, Japan", "Harare, Zimbabwe", 10);
+    //worldAirline.printAdjList();
+
+    worldAirline.testOne("Seoul, South Korea", "Seattle, United States", 10);
+    worldAirline.testOne("Seoul, South Korea", "Panama City, Panama", 10);
+    worldAirline.testOne("Osaka, Japan", "Pittsburgh, United States", 20);
+
+    //worldAirline.testTwo("Osaka, Japan", "Detroit, United States","Vienna, Austria","Harare, Zimbabwe");
+
+    //worldAirline.testTwo("Osaka, Japan", "Caracas, Venezuela","Vienna, Austria","Harare, Zimbabwe");
 
 
     return 0;
